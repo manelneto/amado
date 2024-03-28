19bab297833a5c9bb81183c1d3fc259eac947d43
Manuel Neto <manuelrlcneto@gmail.com>
2024-03-28 20:44:28 +0000

    bugs solving and code organization/cleaning

--- gui.py
+++ gui.py
@@ -2,11 +2,11 @@ from amado import Amado
 import algorithms
 import levels
 import pygame
-import sys
 import time
 
+
 class BaseGameScreen:
-    def __init__(self, screen_width: int = 1200, screen_height: int = 750, change_state_callback=None):
+    def __init__(self, screen_width: int = 1200, screen_height: int = 750, change_state_callback = None):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.change_state_callback = change_state_callback
@@ -30,18 +30,15 @@ class BaseGameScreen:
         self.screen.fill(self.background_color)
         pygame.display.flip()
 
-    # game_board -> True to highlight the selected cell, False otherwise
-    def draw_board(self, pos_x, pos_y, board, scale, game_board = False):
+    def draw_board(self, pos_x: int, pos_y: int, board: list, scale: int, highlight: bool = False):
         board_size = len(board)
         cell_size = self.game_cell_size * scale
-        left_x = pos_x
-        top_y = pos_y
 
         for y in range(board_size):
             for x in range(board_size):
-                rect = pygame.Rect(left_x + x*cell_size, top_y + y*cell_size, cell_size, cell_size)
+                rect = pygame.Rect(pos_x + x * cell_size, pos_y + y * cell_size, cell_size, cell_size)
                 pygame.draw.rect(self.screen, self.colors[board[y][x]], rect)
-                if game_board and self.game_state and y == self.game_state.row and x == self.game_state.col:
+                if highlight and self.game_state and y == self.game_state.row and x == self.game_state.col:
                     pygame.draw.rect(self.screen, self.highlight_color, rect, 5)
 
 
@@ -54,37 +51,23 @@ class GUI(BaseGameScreen):
         self.move_counter = 0
         self.goal_board = levels.GOALS[level]
         
-        # True if algorithm is running , False if player is playing
         self.bot_playing = False
-        # List of plays to be made by the bot
         self.bot_plays = []
-        # Index of current bot play
         self.bot_play_index = 0
-        # Hash Map with buttons needed
-        self.button_rect = {}
-        # Either "algorithms" or "algorithm_menu"
-        self.current_menu = ""
-
-    def draw_level_info(self):
-        # draw the red line
-        pygame.draw.line(self.screen, self.colors['r'], (self.screen_width - 300, 0), (self.screen_width - 300, self.screen_height), 5)
         
-        # draw current level
-        counter_surface = self.font.render("Level " + str(self.level), True, self.highlight_color)
-        counter_position = (10, 20)
-        self.screen.blit(counter_surface, counter_position)
-
-        # draw move counter
-        counter_surface = self.font.render(str(self.move_counter), True, (255, 255, 255))
-        counter_position = (int(self.screen_width / 3), 20)
-        self.screen.blit(counter_surface, counter_position)
-
-        # draw "ESC to get back"
-        counter_surface = pygame.font.SysFont('Arial', 25).render("Press ESC for Menu" , True, (255, 255, 255))
-        counter_position = (10, self.screen_height - 30)
-        self.screen.blit(counter_surface, counter_position)
+        self.algorithm_menu = False
+        self.button_rect = {}
+        self.algorithms = [
+            {"name": "Breadth First Search", "key": "bfs", "position": (930, 250)},
+            {"name": "Depth First Search", "key": "dfs", "position": (930, 300)},
+            {"name": "Depth Limited Search", "key": "dls", "position": (930, 350)},
+            {"name": "Iterative Deepening Search", "key": "ids", "position": (930, 400)},
+            {"name": "Greedy Search", "key": "gs", "position": (930, 450)},
+            {"name": "A*", "key": "astar", "position": (930, 500)}
+        ]
 
     def draw_algorithm_menu(self):
+        self.algorithm_menu = True
         mouse_x, mouse_y = pygame.mouse.get_pos()
 
         # Auto Run
@@ -109,7 +92,7 @@ class GUI(BaseGameScreen):
             exit_button = pygame.font.SysFont('Arial', 25).render("Exit Algorithm", True, (255, 0, 0))
             self.screen.blit(exit_button, exit_pos)
 
-        # Arrow right
+        # Right Arrow
         arrow_right = pygame.font.SysFont('Arial', 50).render(">", True, (255, 255, 255))
         arrow_right_pos = (self.screen_width * 0.90, self.screen_height * 0.70)
         arrow_right_rect = arrow_right.get_rect(topleft=arrow_right_pos)
@@ -120,7 +103,7 @@ class GUI(BaseGameScreen):
             arrow_right = pygame.font.SysFont('Arial', 50).render(">", True, (57, 255, 20))
             self.screen.blit(arrow_right, arrow_right_pos)
 
-        # Arrow left
+        # Left Arrow
         arrow_left = pygame.font.SysFont('Arial', 50).render("<", True, (255, 255, 255))
         arrow_left_pos = (self.screen_width * 0.82, self.screen_height * 0.70)
         arrow_left_rect = arrow_left.get_rect(topleft=arrow_left_pos)
@@ -131,44 +114,82 @@ class GUI(BaseGameScreen):
             arrow_left = pygame.font.SysFont('Arial', 50).render("<", True, (57, 255, 20))
             self.screen.blit(arrow_left, arrow_left_pos)
 
-    # Should return False if the game loop should sto and True otherwise
+    def draw_algorithms(self):
+        self.algorithm_menu = False
+        
+        for algorithm in self.algorithms:
+            self.draw_algorithm_button(algorithm, algorithm['position'])
+
+    def draw_algorithm_button(self, algorithm: dict, position: tuple):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        default_color = (255, 255, 255)
+        hover_color = (57, 255, 20)
+        
+        algo_surface = pygame.font.SysFont('Arial', 25).render(algorithm['name'], True, default_color)
+        algo_rect = algo_surface.get_rect(topleft=position)
+        
+        # If mouse hovers over the button, change the color
+        if algo_rect.collidepoint(mouse_x, mouse_y):
+            algo_surface = pygame.font.SysFont('Arial', 25).render(algorithm['name'], True, hover_color)
+        
+        self.screen.blit(algo_surface, position)
+        self.button_rect[algorithm['key']] = algo_rect  # Store the button rect for click detection
+
+    def draw_level_info(self):
+        # Red Line
+        pygame.draw.line(self.screen, self.colors['r'], (self.screen_width - 300, 0), (self.screen_width - 300, self.screen_height), 5)
+        
+        # Level N
+        counter_surface = self.font.render("Level " + str(self.level), True, self.highlight_color)
+        counter_position = (10, 20)
+        self.screen.blit(counter_surface, counter_position)
+
+        # Move Counter
+        counter_surface = self.font.render(str(self.move_counter), True, (255, 255, 255))
+        counter_position = (int(self.screen_width / 3), 20)
+        self.screen.blit(counter_surface, counter_position)
+
+        # Press ESC for Menu
+        counter_surface = pygame.font.SysFont('Arial', 25).render("Press ESC for Menu" , True, (255, 255, 255))
+        counter_position = (10, self.screen_height - 30)
+        self.screen.blit(counter_surface, counter_position)
+
+    # Returns False if the game loop should stop, and True otherwise
     def update(self) -> bool:
+        if algorithms.goal_test(self.game_state, self.goal_board):
+            time.sleep(0.5)
+            self.change_state_callback('win', self.level, self.move_counter)
+            return True
+
         mouse_x, mouse_y = pygame.mouse.get_pos()
         new_game_state = self.game_state
 
         if self.bot_playing:
-            if self.bot_play_index - 1 != len(self.bot_plays):
-                self.bot_play_index += 1
-                new_game_state = self.bot_plays[self.bot_play_index]
-                time.sleep(1)
-            else:
-                self.bot_playing = False
-                self.bot_plays = []
-                # Exit game for now because there is no winning condition yet
-                pygame.quit()
-                sys.exit()
-                
+            self.bot_play_index += 1
+            new_game_state = self.bot_plays[self.bot_play_index]
+            time.sleep(1)
+        
         else:
             for event in pygame.event.get():
-
                 if event.type == pygame.QUIT:
                     return False
                 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.current_menu == "algorithm_menu":
+                    if self.algorithm_menu:
                         if self.button_rect.get("auto_run") and self.button_rect["auto_run"].collidepoint(mouse_x, mouse_y):
                             self.bot_playing = True
                         elif self.button_rect.get("exit_button") and self.button_rect["exit_button"].collidepoint(mouse_x, mouse_y):
+                            self.bot_play_index = 0
                             self.bot_plays = []
                         elif self.button_rect.get("arrow_right") and self.button_rect["arrow_right"].collidepoint(mouse_x, mouse_y):
                             self.bot_play_index += 1
                             new_game_state = self.bot_plays[self.bot_play_index]
                         elif self.move_counter > 0 and self.button_rect.get("arrow_left") and self.button_rect["arrow_left"].collidepoint(mouse_x, mouse_y):
                             self.bot_play_index -= 1
-                            self.move_counter -= 2 # because 1 is added by default at end
+                            self.move_counter -= 2
                             new_game_state = self.bot_plays[self.bot_play_index]
                     else:
-                        # select the algorithm
                         if self.button_rect.get("bfs") and self.button_rect["bfs"].collidepoint(mouse_x, mouse_y):
                             self.bot_plays = algorithms.breadth_first_search(self.game_state, self.goal_board)
                         elif self.button_rect.get("dfs") and self.button_rect["dfs"].collidepoint(mouse_x, mouse_y):
@@ -176,12 +197,12 @@ class GUI(BaseGameScreen):
                         elif self.button_rect.get("dls") and self.button_rect["dls"].collidepoint(mouse_x, mouse_y):
                             self.bot_plays = algorithms.depth_limited_search(self.game_state, self.goal_board)
                         elif self.button_rect.get("ids") and self.button_rect["ids"].collidepoint(mouse_x, mouse_y):
-                            self.bot_plays = algorithms.iterative_deepening_search(self.game_state, self.goal_board)
+                            self.bot_plays = algorithms.iterative_deepening_search(self.game_state, self.goal_board, 20)
                         elif self.button_rect.get("gs") and self.button_rect["gs"].collidepoint(mouse_x, mouse_y):
                             self.bot_plays = algorithms.greedy_search(self.game_state, self.goal_board)
                         elif self.button_rect.get("astar") and self.button_rect["astar"].collidepoint(mouse_x, mouse_y):
                             self.bot_plays = algorithms.a_star(self.game_state, self.goal_board)
-                    
+
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP or event.key == pygame.K_w:
                         new_game_state = algorithms.up(self.game_state)
@@ -202,20 +223,17 @@ class GUI(BaseGameScreen):
             self.game_state = new_game_state
             self.move_counter += 1
 
-        if algorithms.goal_test(self.game_state, self.goal_board):
-            self.change_state_callback('win', self.level, self.move_counter)
-
         return True
 
     def render(self):
         self.screen.fill(self.background_color)
 
-        # Draw play board
+        # Game Board
         board_x = int(self.screen_width / 3 - (len(self.game_state.board) * self.game_cell_size) / 2)
         board_y = int(self.screen_height / 2 - (len(self.game_state.board) * self.game_cell_size) / 2)
         self.draw_board(board_x, board_y, self.game_state.board, 1, True)
 
-        # Draw goal board
+        # Goal Board
         goal_board_cell_size = int(self.game_cell_size / 2)
 
         if self.level >= 7 and self.level <= 9:
@@ -229,56 +247,25 @@ class GUI(BaseGameScreen):
         top_y = 50
         self.draw_board(left_x, top_y, self.goal_board, scale)
 
-        # Check if algorithm was selected
-        if not self.bot_plays:
-            self.current_menu = "algorithms"
-            self.draw_algorithms()
-        else:
-            self.current_menu = "algorithm_menu"
+        # Side Menu
+        if self.bot_plays:
             self.draw_algorithm_menu()
+        else:
+            self.draw_algorithms()
 
-        # Draw extra info
+        # Level Info
         self.draw_level_info()
 
-        # Update the screen
         pygame.display.flip()
 
-    def draw_algorithm_button(self, algo, position):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        # Default color and hover color
-        default_color = (255, 255, 255)
-        hover_color = (57, 255, 20)
-        
-        algo_surface = pygame.font.SysFont('Arial', 25).render(algo['name'], True, default_color)
-        algo_rect = algo_surface.get_rect(topleft=position)
-        
-        # If mouse hovers over the button, change the color
-        if algo_rect.collidepoint(mouse_x, mouse_y):
-            algo_surface = pygame.font.SysFont('Arial', 25).render(algo['name'], True, hover_color)
-        
-        self.screen.blit(algo_surface, position)
-        self.button_rect[algo['key']] = algo_rect  # Store the button rect for click detection
-
-    def draw_algorithms(self):
-        algorithms = [
-            {"name": "Breadth First Search", "key": "bfs", "position": (930, 250)},
-            {"name": "Depth First Search", "key": "dfs", "position": (930, 300)},
-            {"name": "Depth Limited Search", "key": "dls", "position": (930, 350)},
-            {"name": "Iterative Deepening Search", "key": "ids", "position": (930, 400)},
-            {"name": "Greedy Search", "key": "gs", "position": (930, 450)},
-            {"name": "A*", "key": "astar", "position": (930, 500)}
-        ]
-        
-        for algo in algorithms:
-            self.draw_algorithm_button(algo, algo['position'])
 
 class MainMenu(BaseGameScreen):
     def __init__(self, change_state_callback):
         super().__init__()
         self.change_state_callback = change_state_callback
-        self.selected_level = 1 # Index of the currently selected level
+        self.selected_level = 1
         self.total_levels = len(levels.STARTS)
-        self.level_cols = 5  # Number of columns to display levels
+        self.level_cols = 5
 
     def draw_main_menu(self):
         # Title
@@ -288,7 +275,7 @@ class MainMenu(BaseGameScreen):
 
         self.screen.blit(title_surface, title_rect)
 
-        # Draw levels
+        # Levels
         self.draw_levels_menu()
 
     def draw_levels_menu(self):
@@ -306,36 +293,26 @@ class MainMenu(BaseGameScreen):
             level_text = f"Level {i}"
             level_surface = level_font.render(level_text, True, (255, 255, 255))
 
-            # Color green for selected level
+            # Highlight selected level
             if i == self.selected_level:
                 level_surface = level_font.render(level_text, True, (0, 255, 0))
 
-            # Calculate the position for the text
+            # Draw level number
             text_x = (col * col_width) + col_width / 2 + margin
             text_y = (row * row_height) + top_margin_offset
             level_rect = level_surface.get_rect(center=(text_x, text_y))
-
-            # Draw the level number
             self.screen.blit(level_surface, level_rect)
 
-            # Retrieve the board for the current level
+            # Draw level board
             level_board = levels.GOALS[i]
-
-            # Calculate the width of the mini board
             mini_board_width = len(level_board[0]) * self.game_cell_size * 0.1
-
-            # Calculate the position for the mini board below the level text, centered
             mini_board_pos_x = text_x - mini_board_width / 2 - 10
-            mini_board_pos_y = text_y + level_surface.get_height() / 2 + 15   # Spacing below the level number text
-
-            # Draw the mini board representation
+            mini_board_pos_y = text_y + level_surface.get_height() / 2 + 15
             mini_board_scale = 0.15
             self.draw_board(mini_board_pos_x, mini_board_pos_y, level_board, mini_board_scale)
 
-
+    # Returns False if the game loop should stop, and True otherwise
     def update(self) -> bool:
-        # Should return False if the loop should stop
-        # And True otherwise
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
@@ -359,11 +336,11 @@ class MainMenu(BaseGameScreen):
 
         return True
 
-
     def render(self):
         self.screen.fill(self.background_color)
         self.draw_main_menu()
         pygame.display.flip()
+
 
 class WinMenu(BaseGameScreen):
     def __init__(self, level: int, score: int, change_state_callback):
@@ -383,16 +360,16 @@ class WinMenu(BaseGameScreen):
         score_text_pos = (self.screen_width / 2 - score_text.get_width() / 2, 300)
         self.screen.blit(score_text, score_text_pos)
 
-        # draw "ESC to get back"
+        # Press ESC for Menu"
         counter_surface = pygame.font.SysFont('Arial', 25).render("Press ESC for Menu" , True, (255, 255, 255))
         counter_position = (10, self.screen_height - 30)
         self.screen.blit(counter_surface, counter_position)
 
+    # Returns False if the game loop should stop, and True otherwise
     def update(self) -> bool:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-            # if user presses key, go back to main menu
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN or event.key == pygame.K_ESCAPE:
                     self.change_state_callback('menu')
