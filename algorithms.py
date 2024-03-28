c1d26ceb81ce774198d5c268db9e6c13cc71ca21
Manuel Neto <manuelrlcneto@gmail.com>
2024-03-28 16:17:27 +0000

    added heuristics

--- algorithms.py
+++ algorithms.py
@@ -155,7 +155,7 @@ def greedy_search(initial_state: Amado, goal_board: list):
                 queue.append((new_node, evaluation))
                 visited_states.add(state)
         
-        queue = deque(sorted(queue, key = lambda node: node[1]))
+        queue = deque(sorted(queue, key = lambda element: element[1]))
         
     return None
 
@@ -170,20 +170,67 @@ def get_solution(node: TreeNode):
     print(i)
     return solution
 
+def dfs(board: list, row: int, col: int):
+    queue = deque([(row, col)])
+    group_size = 0
+
+    while queue:
+        row, col = queue.popleft()
+        board[row][col] = False
+        group_size += 1
+
+        if row > 0 and board[row - 1][col]:
+            queue.appendleft((row - 1, col))
+
+        if row < len(board) - 1 and board[row + 1][col]:
+            queue.appendleft((row + 1, col))
+
+        if col > 0 and board[row][col - 1]:
+            queue.appendleft((row, col - 1))
+
+        if col < len(board) - 1 and board[row][col + 1]:
+            queue.appendleft((row, col + 1))
+
+    return group_size
+
 def heuristic(game_state: Amado, goal_board: list):
     # MINIMIZAR
     different_squares = 0
     distances = []
+    new_board = []
+    n_squares = 0
 
     for row in range(game_state.board_size):
+        new_row = []
         for col in range(game_state.board_size):
             if game_state.board[row][col] != goal_board[row][col]:
                 different_squares += 1
                 distance = abs(game_state.row - row) + abs(game_state.col - col)
                 distances.append(distance)
+                new_row.append(True)
+            else:
+                new_row.append(False)
 
-    average = 0
-    if distances:
-        average = sum(distances)/len(distances)
+            if game_state.board[row][col] != 'n':
+                n_squares += 1
+        new_board.append(new_row)
 
-    return different_squares + average
+    groups = []
+    for row in range(len(new_board)):
+        for col in range(len(new_board)):
+            if new_board[row][col]:
+                groups.append(dfs(new_board, row, col))
+
+    max_group = 0
+    if groups:
+        max_group = max(groups)
+
+    different_squares /= n_squares
+    max_group /= n_squares
+    n_groups = len(groups) / n_squares / 2
+
+    p1 = min(different_squares, max_group, n_groups)
+    p3 = max(different_squares, max_group, n_groups)
+    p2 = different_squares + max_group + n_groups - p1 - p3
+
+    return 0.5 * p1 + 0.3 * p2 + 0.2 * p3
