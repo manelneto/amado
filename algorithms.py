74f032ffe3065bcf66e43fc5cd2e163f5cd5c0f3
Matilde Simões <up202108782@up.pt>
2024-03-30 22:24:21 +0000

    added document for algorithms

--- algorithms.py
+++ algorithms.py
@@ -6,11 +6,32 @@ from collections import defaultdict
 # Goal Test
 
 def goal_test(game_state: Amado, goal_board: list) -> bool:
+    """
+    Checks if the current board state matches the goal state.
+
+    Parameters:
+        game_state (Amado): The current game state.
+        goal_board (list): The goal state of the board.
+
+    Returns:
+        bool: True if the current board state matches the goal state, False otherwise.
+    """
     return game_state.board == goal_board
 
 # Operators
 
 def move(game_state: Amado, row: int, col: int) -> Amado:
+    """
+    Performs a move by changing the color of a piece on the board, if possible.
+
+    Parameters:
+        game_state (Amado): The current game state.
+        row (int): The row of the piece to move.
+        col (int): The column of the piece to move.
+
+    Returns:
+        Amado: A new game state after the move, or the same state if the move is not possible.
+    """
     if game_state.can_move(row, col):
         color1 = game_state.current_color()
         color2 = game_state.color(row, col)
@@ -22,20 +43,65 @@ def move(game_state: Amado, row: int, col: int) -> Amado:
         return game_state
 
 def up(game_state: Amado) -> Amado:
+    """
+    Moves the selected piece up on the board, if possible.
+
+    Parameters:
+        game_state (Amado): The current game state.
+
+    Returns:
+        Amado: A new game state after moving the piece up.
+    """
     return move(game_state, game_state.row - 1, game_state.col)
 
 def down(game_state: Amado) -> Amado:
+    """
+    Moves the selected piece down on the board, if possible.
+
+    Parameters:
+        game_state (Amado): The current game state.
+
+    Returns:
+        Amado: A new game state after moving the piece up.
+    """
     return move(game_state, game_state.row + 1, game_state.col)
 
 def left(game_state: Amado) -> Amado:
+    """
+    Moves the selected piece left on the board, if possible.
+
+    Parameters:
+        game_state (Amado): The current game state.
+
+    Returns:
+        Amado: A new game state after moving the piece up.
+    """
     return move(game_state, game_state.row, game_state.col - 1)
 
 def right(game_state: Amado) -> Amado:
+    """
+    Moves the selected piece right on the board, if possible.
+
+    Parameters:
+        game_state (Amado): The current game state.
+
+    Returns:
+        Amado: A new game state after moving the piece up.
+    """
     return move(game_state, game_state.row, game_state.col + 1)
 
 # Child Nodes
 
 def child_game_states(game_state: Amado) -> list:
+    """
+    Generates all possible child states from the current game state.
+
+    Parameters:
+        game_state (Amado): The current game state.
+
+    Returns:
+        list: A list containing all possible child states.
+    """
     new_states = []
 
     move_up = up(game_state)
@@ -56,12 +122,27 @@ def child_game_states(game_state: Amado) -> list:
 
 
 class TreeNode:
+    """
+    Represents a node in the search tree for games.
+    
+    Attributes:
+        game_state (Amado): The game state associated with this node.
+        parent (TreeNode, optional): The parent node of this node in the search tree.
+        children (list): A list of child nodes.
+        depth (int): The depth of this node in the search tree.
+    """
     def __init__(self, game_state: Amado, parent = None):
         self.game_state = game_state
         self.parent = parent
         self.children = []
         self.depth = 0
 
+    """
+    Adds a child node to this node.
+    
+    Parameters:
+        child_node (TreeNode): The child node to be added.
+    """
     def add_child(self, child_node):
         self.children.append(child_node)
         child_node.parent = self
@@ -76,6 +157,16 @@ class TreeNode:
         return self.depth == other.depth and self.game_state == other.game_state
 
 def breadth_first_search(initial_state: Amado, goal_board: list) -> tuple | None:
+    """
+    Performs breadth-first search from an initial state to a goal state.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+
+    Returns:
+        tuple | None: A tuple containing the solution and search statistics, or None if a solution is not found.
+    """
     root = TreeNode(initial_state)
     queue = deque([root])
     visited_states = set([initial_state])
@@ -99,6 +190,16 @@ def breadth_first_search(initial_state: Amado, goal_board: list) -> tuple | None
     return None, depth_count
 
 def depth_first_search(initial_state: Amado, goal_board: list) -> tuple | None:
+    """
+    Performs depth-first search from an initial state to a goal state.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+
+    Returns:
+        tuple | None: A tuple containing the solution path (if found) and search statistics, or None if a solution is not found.
+    """
     root = TreeNode(initial_state)
     queue = deque([root])
     visited_states = set([initial_state])
@@ -121,6 +222,17 @@ def depth_first_search(initial_state: Amado, goal_board: list) -> tuple | None:
     return None, depth_count
 
 def depth_limited_search(initial_state: Amado, goal_board: list, depth_limit: int) -> tuple | None:
+    """
+    Performs depth-limited search from an initial state to a goal state, up to a specified depth.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+        depth_limit (int): The maximum depth for the search.
+
+    Returns:
+        tuple | None: A tuple containing the solution path (if found) and search statistics, or None if a solution is not found within the depth limit.
+    """
     root = TreeNode(initial_state)
     queue = deque([root])
     visited_nodes = set()
@@ -146,6 +258,17 @@ def depth_limited_search(initial_state: Amado, goal_board: list, depth_limit: in
     return None, depth_count
 
 def iterative_deepening_search(initial_state: Amado, goal_board: list, depth_limit: int) -> tuple | None:
+    """
+    Performs iterative deepening search from an initial state to a goal state. This method incrementally increases the depth limit until the goal is found or the depth limit is reached.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+        depth_limit (int): The maximum depth limit to reach in iterative deepening.
+
+    Returns:
+        tuple | None: A tuple containing the solution path (if found) and aggregated search statistics across all depths, or None if a solution is not found within the depth limit.
+    """
     depth_count = defaultdict(int)
     for i in range(depth_limit + 1):
         result = depth_limited_search(initial_state, goal_board, i)
@@ -154,6 +277,17 @@ def iterative_deepening_search(initial_state: Amado, goal_board: list, depth_lim
     return None
 
 def greedy_search(initial_state: Amado, goal_board: list, heuristic_num : int = 4) -> tuple | None:
+    """
+    Performs greedy search from an initial state to a goal state, using a heuristic to prioritize nodes.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+        heuristic_num (int, optional): The number indicating which heuristic to use for prioritizing nodes.
+
+    Returns:
+        tuple | None: A tuple containing the solution path (if found) and search statistics, or None if a solution is not found.
+    """
     root = TreeNode(initial_state)
     queue = deque([(root, heuristic(initial_state, goal_board, heuristic_num))])
     visited_states = set([initial_state])
@@ -181,6 +315,18 @@ def greedy_search(initial_state: Amado, goal_board: list, heuristic_num : int = 
     return None, depth_count
 
 def a_star(initial_state: Amado, goal_board: list, weight: int = 1, heuristic_num : int = 4) -> tuple | None:
+    """
+    Performs A* search from an initial state to a goal state. Combines the cost to reach the node and a heuristic estimate of the cost to reach the goal.
+
+    Parameters:
+        initial_state (Amado): The initial game state.
+        goal_board (list): The goal state of the board.
+        weight (int, optional): The weight given to the heuristic component of the cost.
+        heuristic_num (int, optional): The number indicating which heuristic to use for estimating the cost to reach the goal.
+
+    Returns:
+        tuple | None: A tuple containing the solution path (if found) and search statistics, or None if a solution is not found.
+    """
     root = TreeNode(initial_state)
     queue = deque([(root, heuristic(initial_state, goal_board, heuristic_num))])
     visited_states = set([initial_state])
@@ -241,6 +387,17 @@ def visit_group(board: list, row: int, col: int) -> int:
     return group_size
 
 def heuristic(game_state: Amado, goal_board: list, choice: int) -> int:
+    """
+    Calculates the heuristic estimate from the current state to the goal state.
+
+    Parameters:
+        game_state (Amado): The current game state.
+        goal_board (list): The goal state of the board.
+        choice (int): The number indicating the heuristic strategy to be used.
+
+    Returns:
+        int: The calculated heuristic value.
+    """
     different_squares = 0
     new_board = []
     n_squares = 0
